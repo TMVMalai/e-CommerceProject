e2fa890b56e0151be1a593217d05091063ddbcfa
Thirumalaivasan <tperumal2205@gmail.com>
2024-01-19 11:03:56 +0530

    Final changes

--- src/test/java/baseFunctions/CommonFunctions.java
+++ src/test/java/baseFunctions/CommonFunctions.java
@@ -3,16 +3,16 @@ package baseFunctions;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+
 
 public class CommonFunctions extends utils.UtilityClasses {
 	
-	@BeforeSuite(alwaysRun=true)
+	@BeforeMethod(alwaysRun=true)
 	public void suiteSetup() throws IOException {
 		driver = initialization();
 	}
